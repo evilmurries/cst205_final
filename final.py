75a4d4e70c9c53cdcdfcad44ccdcc16352d476d0
Mark Mariscal <markamariscal@gmail.com>
2018-12-03 16:48:41 -0800

    Update final.py

--- final.py
+++ final.py
@@ -5,6 +5,16 @@
 from javax.swing import JFrame, JButton
 from java.awt import GridLayout
 
+welcomeMessage ='Welcome to Animal Sounds Tic Tac Toe'
+helpMessage = 'A two player game of tic tac toe where players win over squares by correctly guessing the animal sound sample.Each correct guess displays the animal represented by the sound.An incorrect guess results in the square being left open.The first player to complete a row of three for their shape wins!'
+   
+#Welcome message for our game
+showInformation(welcomeMessage)
+showInformation(helpMessage)
+
+
+
+
 frame = JFrame("Animal Sounds Tic Tac Toe")
 #frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 frame.setLocation(100,100)
