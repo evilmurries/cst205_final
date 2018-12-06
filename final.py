a77dbd5aae1bc8c020bc8eab3b1816a7b55646ad
evilmurries <cpiwarski00@gmail.com>
2018-12-06 10:15:04 -0800

    workin on animal stuff

--- final.py
+++ final.py
@@ -49,8 +49,8 @@ class Animal:
   # Constructor method for class Animal
   def __init__(self, name='', sound=None, picture=None):  
     self.name = name
-    self.sound = makeSound(sound)
-    self.picture = makePicture(picture)
+    self.sound = sound
+    self.picture = picture
 
   # This method returns the name for the animal object
   def getName(self):
@@ -78,13 +78,52 @@ class Animal:
 
   def __str__(self):
     print self.name
-    explore(self.sound)
-    explore(self.picture)
+
+################################################################################
+# functions
+################################################################################
+
+def makeAnimals():
+
+  animals = []
+  # create animal objects
+  bear = Animal('bear')
+  bird = Animal('bird')
+  cat = Animal('cat')
+  chicken = Animal('chicken')
+  cow = Animal('cow')
+  cricket = Animal('cricket')
+  dog = Animal('dog')
+  dolphin = Animal('dolphin')
+  donkey = Animal('donkey')
+  horse = Animal('horse')
+  monkey = Animal('monkey')
+  pig = Animal('pig')
+  seal = Animal('seal')
+  whale = Animal('whale')
+
+  # add animals to the list
+  animals.append(bear)
+  animals.append(bird)
+  animals.append(cat)
+  animals.append(chicken)
+  animals.append(cow)
+  animals.append(cricket)
+  animals.append(dog)
+  animals.append(dolphin)
+  animals.append(donkey)
+  animals.append(horse)
+  animals.append(monkey)
+  animals.append(pig)
+  animals.append(seal)
+  animals.append(whale)
+  return animals
 
 
-###############################################################################
+
+################################################################################
 # Main Game Script
-###############################################################################
+################################################################################
 
 welcomeMessage ='Welcome to Animal Sounds Tic Tac Toe'
 helpMessage = 'A two player game of tic tac toe where players win over squares\
@@ -110,11 +149,9 @@ showInformation('Welcome %s and %s! Lets begin the game' % (player1.getName(),\
 # Does this all belong in the main game loop? Like the text adventure?
 
 # here we can create the list of animals, example below
-path = os.path.abspath('.')
-animalList = []
-#dog = Animal('dog', (path + 'animal sounds/dog_bark2.wav'), path + 'peek.jpg')
-animalList.append(dog)
-
+animalList = makeAnimals()
+for animal in animalList:
+  printNow(animal.getName())
 
 #tic tac toe board with individual buttons(I need to make them bigger though)
 
